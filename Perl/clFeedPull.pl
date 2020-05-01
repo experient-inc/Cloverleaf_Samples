42277c0785f98dff415eeb5b2deff5e559a83e0d
jdm <jdm@jdmcentos8.macnett.local>
2020-04-30 23:48:35 -0400

    checkpoint....got baseline version of perl script working and looping through feed; should probably do some more cleanup around argument processing and the like

--- Perl/clFeedPull.pl
+++ Perl/clFeedPull.pl
@@ -7,6 +7,10 @@ use JSON;
 use MIME::Base64 qw(encode_base64);
 use REST::Client;
 
+# Required CPAN installations (if not already present):
+# JSON
+# REST::Client
+
 # parse arguments
 scalar(@ARGV) < 4 and die("usage: $0 <EventCode> <FeedType> <clUserName> <clPassword> [<Since>]");
 my $eventCode = shift(@ARGV);
@@ -21,7 +25,14 @@ if( scalar(@ARGV) == 6) {
 
 # get path for output file
 (my $sec,my $min,my $hour,my $mday,my $mon,my $year,my $wday,my $yday,my $isdst) = localtime();
-my $out = getcwd() . "/" . $eventCode . "_" . $feedType . "_" . $year . $mon . $mday . $hour . $min . $sec . ".txt";
+my $odir = getcwd() . "/feedRes";
+if (-e $odir and -d $odir) {
+    print "Feed output directory already exists\n";
+} else {
+    print "Creating feed output directory...\n";
+    mkdir $odir;
+}
+my $out = $odir . "/" . $eventCode . "_" . $feedType . "_" . $year . $mon . $mday . $hour . $min . $sec . ".json";
 print "Output file:\n$out\n";
 
 # start loop
@@ -35,20 +46,55 @@ my $client = REST::Client->new();
 my $joinedPW = encode_base64(encode('ASCII', "$clUserName:$clPassword"));
 $client->addHeader('Authorization',"Basic $joinedPW");
 
-print "$joinedPW\n";
-
 # loop through data
 my $pull = undef;
+my $startEntry = 0;
+my $allRecordCnt = 0;
+my $entCnt = 0;
+
+# open output file for write
+open(my $fh, '>>:encoding(UTF-8)', $out) or die "Could not open file '$out' $!";
+print $fh "[";
+
 if($sinceVal && $sinceVal > 0) { $nextSince = $sinceVal; }
-# do
-# {
+do
+{
     my $url =  "https://api.experientdata.com/Feed$feedType?Event=$eventCode";
     if($nextSince) {
-        $url += "&Since=" . $nextSince;
+        $url = $url . "&Since=" . $nextSince;
     }
     print "$url\n";
 
     $pull = $client->GET($url)->responseContent();
-    print "$pull\n";
-# } 
-# while()
+    my $json = from_json($pull);
+
+    my @entArr = @{$json->{Entities}};
+    $entCnt= scalar(@entArr);
+    my $remains = $json->{EstMoreRecords};
+    if(!$remains) {
+        $remains = 0;
+    }
+    print "Retrieved $entCnt records; $remains estimated remaining\n";
+
+    $allRecordCnt += $entCnt;
+
+    $nextSince = undef;
+    $nextSince = $json->{NextSince};
+
+    foreach my $ent(@entArr) {
+        if(!$startEntry) {
+            print $fh to_json($ent);
+            $startEntry = 1;
+        }
+        else {
+            print $fh "," . to_json($ent);
+        }
+        
+    }
+} 
+while($entCnt > 0);
+
+print $fh "]";
+close $fh;
+
+print "Feed complete! $allRecordCnt records retrieved.\n";
