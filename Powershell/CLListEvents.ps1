93c60cc1b9ed42a9f05e283dd8194257ffdec045
Jesse MacNett <jesse.macnett@experient-inc.com>
2020-07-31 13:52:17 -0400

    powershell script for pulling event list

---
+++ Powershell/CLListEvents.ps1
@@ -0,0 +1,42 @@
+
+param 
+(
+    [Parameter(Mandatory=$true)]
+    [string] $clUserName,
+    [Parameter()]
+    [string] $clPassword,
+    [Parameter()]
+    [string] $out
+)
+
+if($host.version.Major -lt 6) {
+    throw "Powershell 6 or higher is required to run this script!  Execution cannot continue.  Powershell 6 is available here: https://aka.ms/pscore6 "
+    return;
+}
+
+[SecureString] $pass = $null;
+if(!$clPassWord) {
+    $pass = Read-Host -AsSecureString -Prompt "Enter your cloverleaf password"
+}
+else {
+    $pass = convertto-securestring -string $clPassword -AsPlainText -Force
+}
+$clCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $clUserName, $pass
+
+if(!$out) {
+    $out = (get-location).Path + "\feedRes\" + $EventCode + "_" + $FeedType + "_" + [DateTime]::Now.ToString("yyyyMMddHHmmssfff") + ".json";
+
+    if(!(test-path ((get-location).Path + "\feedRes\"))) {
+        $x = mkdir feedRes;
+    }
+}
+"Output file: $out";
+
+$url = ("https://api.experientdata.com/FeedEvent");
+
+$pull = $null;
+$pull = Invoke-RestMethod -uri $url -Method GET -Authentication Basic -Credential $clCreds -AllowUnencryptedAuthentication
+
+$pull
+
+#add-content -path $out -Value ("{0}" -f $pull.ToString())
